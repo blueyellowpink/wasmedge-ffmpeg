70a7ab0611766857bd7ae59a5744d3813bc605e1
blueyellowpink <andrew@trasua.dev>
2023-08-21 01:22:34 +0700

    update build script

--- build_local.sh
+++ build_local.sh
@@ -4,12 +4,7 @@ export LLVM_DIR=$(brew --prefix)/opt/llvm/lib/cmake
 export CC=$(brew --prefix)/opt/llvm/bin/clang
 export CXX=$(brew --prefix)/opt/llvm/bin/clang++
 
-echo $LLVM_DIR
-echo $CC
-echo $CXX
-
 cmake -Bbuild \
-      -Wno-unused-parameter \
       -GNinja \
       -DWASMEDGE_BUILD_TESTS=OFF \
       -DWASMEDGE_PLUGIN_FFMPEG=ON \
